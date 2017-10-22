e292cf864a389a44df427c906094cefc6b41845d
fabien.lehouedec@imie-rennes.fr <fabien.lehouedec@imie-rennes.fr>
2017-10-22 22:47:33 +0200

    Création de la structure du projet Java pour le back du projet d'annonces

---
+++ src/main/java/imie/annonces/ws/AnnoncesWS.java
@@ -0,0 +1,12 @@
+package imie.annonces.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnoncesWS {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnoncesWS.class, args);
+	}
+}
